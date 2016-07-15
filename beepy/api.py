6a986303cdadc40d2b784dba4ab8d8187c35864e
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-07-15 17:29:36 +0900

    implement query API

--- beepy/api.py
+++ beepy/api.py
@@ -2,7 +2,9 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import contextlib
 import json
+import email.parser
 
 try:
   from urllib.request import urlopen
@@ -14,9 +16,11 @@ class BeePyAPI(object):
     self.host = host
     self.port = port
 
+  def _url(self, path):
+    return 'http://{0}:{1}/api/v1/{2}'.format(self.host, self.port, path)
+
   def _req(self, path):
-    url = 'http://{0}:{1}/api/v1/{2}'.format(self.host, self.port, path)
-    return json.loads(urlopen(url).read().decode())
+    return json.loads(urlopen(self._url(path)).read().decode())
 
   def runtime_status(self):
     return self._req('runtime_status')
@@ -44,3 +48,62 @@ class BeePyAPI(object):
 
   def sink(self, t, s):
     return self._req('topologies/{0}/sinks/{1}'.format(t, s))
+
+  def query(self, t, q):
+    close = False
+    f = urlopen(self._url('topologies/{0}/queries'.format(t)), json.dumps({'queries': q}).encode())
+    close = True
+    try:
+      msg = MessageWrapper(f.info())
+      mimetype = msg.get_content_type()
+      if mimetype == 'application/json':
+        return json.loads(f.read().decode())
+      elif mimetype == 'multipart/mixed':
+        rs = ResultSet(f, msg.get_param('boundary'))
+        close = False
+        return rs
+      else:
+        raise RuntimeError('unexpected MIME type: {0}'.format(mimetype))
+    finally:
+      if close:
+        f.close()
+
+class MessageWrapper(object):
+  def __init__(self, msg):
+    self.msg = msg
+
+  def get_content_type(self, *args, **kwargs):
+    if hasattr(self.msg, 'gettype'):
+      return self.msg.gettype(*args, **kwargs)
+    return self.msg.get_content_type(*args, **kwargs)
+
+  def get_param(self, *args, **kwargs):
+    if hasattr(self.msg, 'getparam'):
+      return self.msg.getparam(*args, **kwargs)
+    return self.msg.get_param(*args, **kwargs)
+
+class ResultSet(object):
+  def __init__(self, _f, _boundary):
+    self._f = _f
+    self._boundary = _boundary
+
+  def __del__(self):
+    f = self._f
+    if f:
+      f.close()
+
+  def __iter__(self):
+    with contextlib.closing(self._f) as f:
+      parser = None
+      while True:
+        if not parser:
+          parser = email.parser.FeedParser()
+        line = f.readline()
+        if not line: break
+        if line.rstrip() == '--{0}'.format(self._boundary).encode():
+          line = f.readline()
+          while line.rstrip():
+            parser.feed(line.decode())
+            line = f.readline()
+          yield json.loads(f.read(int(parser.close()['Content-Length'])).decode())
+          parser = None
